76cbac93c5150a49336b9db0eb30735977fc2420
abirami2505 <181993649+abirami2505@users.noreply.github.com>
2026-01-19 15:56:22 +0530

    Update AttendanceStatusEvaluator.java

--- src/AttendanceStatusEvaluator.java
+++ src/AttendanceStatusEvaluator.java
@@ -1,1 +1,18 @@
-
+import java.util.Scanner;
+public class AttendanceStatusEvaluator{
+   public static void main(String[] args){
+     Scanner sc= new Scanner(System.in);
+     int Attendance = sc.nextInt();
+     if(Attendance>85){
+       System.out.println("Excellent");
+     }
+     else if(Attendance >=60 && Attendance <=85){
+       System.out.println("Satisfactory");
+     }
+     else{
+       System.out.println("Poor");
+     }
+   }
+}
+   
+  
